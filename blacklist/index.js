9c296cdbfcf5b7f5ba0a255bd077151a2edc08ce
fanelfa <ilhamelfaro@gmail.com>
2020-09-04 00:28:47 +0700

    maybe finish

---
+++ blacklist/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const yargs = require("yargs");
+var fs = require("fs");
+
+const options = yargs
+ .usage("Usage:\n-n <name> \n-pn <pnumber>")
+ .option("n", { alias: "name", describe: "The name", type: "string", demandOption: true })
+ .option("p", { alias: "pnumber", describe: "The phone number", type: "int", demandOption: true })
+ .argv;
+
+if(options.name && options.pnumber){
+    console.log(isInBlackList(options.name, options.pnumber));
+}
+
+function readFile(filename = "files/blacklist.txt"){
+    var blacklist_data;
+    try{
+        // console.log("Membaca blacklist yang tersimpan...");
+        blacklist_data = fs.readFileSync(filename, 'utf8').split(/\r?\n/);
+        return blacklist_data;
+    }catch(e){
+        console.log('Error:', e.stack);
+    }
+}
+
+function isInBlackList(uname, uphone){
+    try{
+        test = uname+" "+uphone.toString();
+        console.log(`apakah { ${test} } termasuk dalam blacklist?`);
+        return readFile().includes(test);
+    }catch(e){
+        console.log('Error:', e.stack);
+    }
+}
