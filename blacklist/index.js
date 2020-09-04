6a7660447457116b2c9b223e53769737a00095d0
fanelfa <ilhamelfaro@gmail.com>
2020-09-04 22:10:08 +0700

    clear sync

--- blacklist/index.js
+++ blacklist/index.js
@@ -3,28 +3,34 @@
 const yargs = require("yargs");
 var fs = require("fs");
 
+
+
 const options = yargs
  .usage("Usage:\n-n <name> \n-pn <pnumber>")
  .option("n", { alias: "name", describe: "The name", type: "string", demandOption: true })
  .option("p", { alias: "pnumber", describe: "The phone number", type: "int", demandOption: true })
  .argv;
 
+
+
 if(options.name && options.pnumber){
-    console.log(isInBlackList(options.name, options.pnumber));
+    console.log(check_blacklist(options.name, options.pnumber));
 }
 
-function readFile(filename = "files/blacklist.txt"){
+
+
+function initialize(black_list = "files/blacklist.txt"){
     var blacklist_data;
     try{
         // console.log("Membaca blacklist yang tersimpan...");
-        blacklist_data = fs.readFileSync(filename, 'utf8').split(/\r?\n/);
+        blacklist_data = fs.readFileSync(black_list, 'utf8').split(/\r?\n/);
         return blacklist_data;
     }catch(e){
         console.log('Error:', e.stack);
     }
 }
 
-function isInBlackList(uname, uphone){
+function check_blacklist(uname, uphone){
     try{
         test = uname+" "+uphone.toString();
         console.log(`apakah { ${test} } termasuk dalam blacklist?`);
